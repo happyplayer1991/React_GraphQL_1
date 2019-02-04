2e38a322494e3b94014e9cd925f1ae52317cab9f
freshcoderchan <freshcoderjack@outlook.com>
2019-02-04 22:34:44 +0200

    First step - GraphQL works

--- src/App.js
+++ src/App.js
@@ -1,28 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const axiosGitHubGraphQL = axios.create({
+    baseURL: 'https://api.github.com/graphql',
+    headers: {
+        // Authorization: `bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`
+        Authorization: 'bearer 4c889a0569c1c499d353c87444f3bf50517fd7ef',
+    },
+});
+
+const TITLE = 'React GraphQL GitHub Client';
+
+const GET_ORGANIZATION = `{
+    organization(login: "the-road-to-learn-react") {
+        name
+        url
+    }
+}`;
+
+export class App extends Component {
+    state = {
+        path: 'the-road-to-learn-react/the-road-to-learn-react',
+    };
+
+    componentDidMount = () => {
+        // fetch data
+        this.onFetchFromGitHub();
+    }
+    
+    onChange = event => {
+        this.setState({ path: event.target.value });
+    };
+
+    onSubmit = event => {
+        // fetch data
+        event.preventDefault();
+    };
+
+    onFetchFromGitHub = () => {
+        axiosGitHubGraphQL
+            .post('', { query: GET_ORGANIZATION })
+            .then(result => console.log(result));
+    };
+
+    render() {
+        const { path } = this.state;
+
+        return (
+        <div>
+            <h1>{TITLE}</h1>
+
+            <form onSubmit={this.onSubmit}>
+                <label htmlFor="url">
+                    Show open issues for https://github.com/
+                </label>
+                <input
+                    id="url"
+                    type="text"
+                    value={path}
+                    onChange={this.onChange}
+                    style={{ width: '300px' }}
+                />
+                <button type="submit">Search</button>  
+
+                <hr />
+
+            </form>
+        </div>
+        )
+    };
 }
 
 export default App;
